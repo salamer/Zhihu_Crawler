9e8a1cf64406cb0c89b344380812c4ec4ec0ec94
aljun <salamer_gaga@163.com>
2016-04-07 15:34:28 +0800

    ~~~

--- engine.py
+++ engine.py
@@ -6,7 +6,7 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 import redis
-from crawler import Zhihu_Crawler
+import crawler
 
 #connect to redis server
 red=redis.Redis(host='localhost', port=6379, db=1)
@@ -17,14 +17,14 @@ def check_url(url):
         red.lpush("url_queue",url)
 
 
-if __main__=="__name__":
+if __name__=="__main__":
     i=0
     red.lpush("url_queue","https://www.zhihu.com/people/gaoming623")
     url=red.lpop("url_queue")
     while(url):
         i=i+1
         url=url+'/followees'
-        new_crawler=Zhihu_Crawler(url)
+        new_crawler=crawler.Zhihu_Crawler(url)
         new_crawler.send_request()
         if (i==100):
             break
